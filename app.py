4c5549f2dea1bba864a9a5ceb29befb0f50e45ba
thoratdivya2805-oss <thoratdivya2805@gmail.com>
2026-09-10 15:27:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import joblib
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score 
-from sklearn.metrics import confusion_matrics
+
+
+
+ 
+
 
 # ---------- Page Config ----------
 st.set_page_config(page_title="Customer Churn Prediction", layout="wide")
